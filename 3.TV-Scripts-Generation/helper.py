5faa64ad94a823126c9528adcc73a98e4f186814
geekgineer <abdalrahman.m5959@gmail.com>
2020-04-19 08:05:39 +0200

    added projects

---
+++ 3.TV-Scripts-Generation/helper.py
@@ -0,0 +1,97 @@
+import os
+import pickle
+import torch
+
+
+SPECIAL_WORDS = {'PADDING': '<PAD>'}
+
+
+def load_data(path):
+    """
+    Load Dataset from File
+    """
+    input_file = os.path.join(path)
+    with open(input_file, "r") as f:
+        data = f.read()
+
+    return data
+
+import re
+from collections import Counter
+
+def preprocess(text):
+
+    # Replace punctuation with tokens so we can use them in our model
+    text = text.lower()
+    text = text.replace('.', ' <PERIOD> ')
+    text = text.replace(',', ' <COMMA> ')
+    text = text.replace('"', ' <QUOTATION_MARK> ')
+    text = text.replace(';', ' <SEMICOLON> ')
+    text = text.replace('!', ' <EXCLAMATION_MARK> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    text = text.replace('(', ' <LEFT_PAREN> ')
+    text = text.replace(')', ' <RIGHT_PAREN> ')
+    text = text.replace('--', ' <HYPHENS> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    # text = text.replace('\n', ' <NEW_LINE> ')
+    text = text.replace(':', ' <COLON> ')
+    words = text.split()
+    
+    # Remove all words with  5 or fewer occurences
+    word_counts = Counter(words)
+    trimmed_words = [word for word in words if word_counts[word] > 5]
+
+    return trimmed_words
+
+
+def create_lookup_tables(words):
+    """
+    Create lookup tables for vocabulary
+    :param words: Input list of words
+    :return: Two dictionaries, vocab_to_int, int_to_vocab
+    """
+    word_counts = Counter(words)
+    # sorting the words from most to least frequent in text occurrence
+    sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
+    # create int_to_vocab dictionaries
+    int_to_vocab = {ii: word for ii, word in enumerate(sorted_vocab)}
+    vocab_to_int = {word: ii for ii, word in int_to_vocab.items()}
+
+    return vocab_to_int, int_to_vocab
+
+def preprocess_and_save_data(dataset_path, token_lookup, create_lookup_tables):
+    """
+    Preprocess Text Data
+    """
+    text = load_data(dataset_path)
+    
+    # Ignore notice, since we don't use it for analysing the data
+    text = text[81:]
+
+    token_dict = token_lookup()
+    for key, token in token_dict.items():
+        text = text.replace(key, ' {} '.format(token))
+
+    text = text.lower()
+    text = text.split()
+
+    vocab_to_int, int_to_vocab = create_lookup_tables(text + list(SPECIAL_WORDS.values()))
+    int_text = [vocab_to_int[word] for word in text]
+    pickle.dump((int_text, vocab_to_int, int_to_vocab, token_dict), open('preprocess.p', 'wb'))
+
+
+def load_preprocess():
+    """
+    Load the Preprocessed Training data and return them in batches of <batch_size> or less
+    """
+    return pickle.load(open('preprocess.p', mode='rb'))
+
+
+def save_model(filename, decoder):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    torch.save(decoder, save_filename)
+
+
+def load_model(filename):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    return torch.load(save_filename)
